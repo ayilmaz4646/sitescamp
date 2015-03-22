9766edeb4e22dc9b1fdb73ac1e8972367f75cb9b
Ahmet Yilmaz <ayilmaz4646@gmail.com>
2015-03-22 13:09:40 +0200

    adding readlater page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'home#index'
+  root 'home#index'
 
   %w[roadmap features pricing faq].each do |page|
     get page, controller: "home", action: page
