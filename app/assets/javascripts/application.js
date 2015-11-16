901965baca7b9a7d3f10014d44bcafd341f81508
Ahmet Yilmaz <ahmet.yilmaz@nimbo.com.tr>
2015-11-16 10:37:17 +0200

    edit template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,16 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require local_time
-//= require modernizr.custom.32229-2.8-respondjs-1-4-2
-//= require jquery.jpanelmenu.min
-//= require bootstrap.min
-//= require fastclick.min
-//= require fastclick.min
+//= require writer/modernizr.custom.32229-2.8-respondjs-1-4-2
+//= require writer/jquery-1.11.2.min
+//= require writer/jquery.jpanelmenu.min
+//= require writer/bootstrap.min
+//= require writer/fastclick.min
+//= require writer/main
+// require turbolinks
+// require local_time
+// require modernizr.custom.32229-2.8-respondjs-1-4-2
+// require jquery.jpanelmenu.min
+// require bootstrap.min
+// require fastclick.min
+// require fastclick.min
--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,6 +1,8 @@
 //= require jquery
 //= require jquery_ujs
-//= require modernizr.custom.32229-2.8-respondjs-1-4-2
-//= require jquery.jpanelmenu.min
-//= require bootstrap.min
-//= require fastclick.min
+//= require writer/modernizr.custom.32229-2.8-respondjs-1-4-2
+//= require writer/jquery-1.11.2.min
+//= require writer/jquery.jpanelmenu.min
+//= require writer/bootstrap.min
+//= require writer/fastclick.min
+//= require writer/main
